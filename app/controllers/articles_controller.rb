5145b45a9ae88548df52fb4ec09a5ee478750f66
Andres <andresehenriquezc@gmail.com>
2018-03-30 00:50:28 -0400

    Create and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,22 @@ class ArticlesController < ApplicationController
 
   def create
     # render plain: params[:article].inspect
+    # @article.save
     @article = Article.new (article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+
+    
+  end
+
+  def show
+
+    @article = Article.find(params[:id])
+
   end
 
   private
