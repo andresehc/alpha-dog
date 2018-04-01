ff7c5538973e298faab728b32835f05f411382e0
Andres <andresehenriquezc@gmail.com>
2018-03-31 23:02:05 -0400

    Create users table and model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+
+  # validates :username, presence: true , length: { minimum: 3, maximum: 50}
+  # validates :email, presence: true , length: { minimum: 20, maximum: 500}
+
+end
