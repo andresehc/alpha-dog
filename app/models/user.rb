354761f055cab6c4889b602a5b4437da7755eb5c
Andres <andresehenriquezc@gmail.com>
2018-04-01 01:54:52 -0400

    Associations between users and articles tables and debugger byebug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+  has_many :articles
+  before_save { self.email = email.downcase}
   validates :username, presence: true , 
             # uniqueness: true, 
             uniqueness: {case_sensitive: false }, 
@@ -10,5 +11,6 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
             # uniqueness: true, 
             uniqueness: {case_sensitive: false },
             format: {with: VALID_EMAIL_REGEX}
+  
 
 end
