055d78fb45a41a48be08acb532b1fbb1b072d534
Andres <andresehenriquezc@gmail.com>
2018-04-02 20:24:14 -0400

    prueba users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user,only:[:edit, :update, :show, :destroy]
-  before_action :require_user,except:[:index,:show]
+  # before_action :require_user,except:[:index,:show]
   before_action :require_same_user, only:[:edit]
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
