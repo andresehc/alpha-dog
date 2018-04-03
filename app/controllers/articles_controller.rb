5be50a63d1f96be6a17ca5cd713adc7f6ff33960
Andres <andresehenriquezc@gmail.com>
2018-04-02 20:17:47 -0400

    Complete authentication, including controllers authentication, created current_user to be articles creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article,only:[:edit, :update, :show, :destroy]
-
+  before_action :require_user,except:[:index,:show]
+  before_action :require_same_user, only:[:edit,:update,:destroy]
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
@@ -13,7 +14,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     # @article.save
     @article = Article.new (article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was succesfully created"
       redirect_to article_path(@article)
@@ -55,5 +56,11 @@ class ArticlesController < ApplicationController
 
     def article_params
       params.required(:article).permit(:title, :description)
+    end
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit or delete your own articles"
+        redirect_to root_path
+      end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-   before_action :set_user,only:[:edit, :update, :show, :destroy]
+  before_action :set_user,only:[:edit, :update, :show, :destroy]
+  before_action :require_user,except:[:index,:show]
+  before_action :require_same_user, only:[:edit]
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
   end
@@ -54,7 +56,15 @@ class UsersController < ApplicationController
   def user_params
     params.required(:user).permit(:username, :email, :password)
   end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own user"
+      redirect_to root_path
+    end
+  end
 end
+
 
 
 
