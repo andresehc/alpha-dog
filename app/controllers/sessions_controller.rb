a44fb43daaf0d7623116a4c09ff779a5a85e4b87
Andres <andresehenriquezc@gmail.com>
2018-04-03 00:13:17 -0400

    Added admin funcionalities

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in" + user.username
+      flash[:success] =  current_user.username + ", you have successfully logged in!" 
       redirect_to user_path(user)
     else
       flash.now[:danger] = "There was something wrong with your login. Try again!"
