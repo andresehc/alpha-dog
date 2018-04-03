6794e336ade9493bc40c8f18c8e9e366d6dfc6e4
Andres <andresehenriquezc@gmail.com>
2018-04-03 01:17:42 -0400

    Delete users by admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user,only:[:edit, :update, :show, :destroy]
+  before_action :set_user,only:[:edit, :update, :show]
   # before_action :require_user,except:[:index,:show]  Aqui no se usa
-  before_action :require_same_user, only:[:edit]
+  before_action :require_same_user, only:[:edit, :destroy]
+  before_action :require_admin, only:[:destroy]
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
   end
@@ -36,7 +37,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    flash[:danger] = "The User was eliminated succesfully"
+    flash[:danger] = "The User and all the articles created by user were eliminated succesfully"
     redirect_to articles_path
   end
   
@@ -59,8 +60,15 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:danger] = "You can only edit your own user"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if current_user != @user && !current_user.admin?
+      flash[:danger] = "Warning! Only Admin users can perform that action"
       redirect_to root_path
     end
   end
